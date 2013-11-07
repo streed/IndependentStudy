c9046a8c15ea75dee037e50d100998235a5197f9
streed <sean@cheapfit.me>
2013-11-07 13:47:19 -0500

    A lot of html and js

--- app/blueprints/driver.py
+++ app/blueprints/driver.py
@@ -1,12 +1,28 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, redirect
 from flask.ext.security import login_required
+from flask.ext.security.core import current_user
 from flask.ext.security.decorators import roles_required, roles_accepted
+
+from ..models.account import accounts
+from ..db import db
 
 driver = Blueprint( "driver", __name__, template_folder="templates" )
 
 @driver.route( "/" )
 @login_required
-@roles_accepted( "Admin", "Driver" )
 def index():
-	return render_template( "driver/index.html" )
+	if( current_user.has_role( "Driver" ) ):
+		return render_template( "driver/index.html" )
+	else:
+		accounts.add_role_to_user( current_user, "Driver" )
+		db.session.commit()
+		
+		return render_template( "driver/sign_up.html" )
 
+@driver.route( "/schedule" )
+@login_required
+def schedule():
+	if( current_user.has_role( "Driver" ) ):
+		return render_template( "driver/schedule.html" )
+	else:
+		return redirect( "/" )
